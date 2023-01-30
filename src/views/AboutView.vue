b0805fa56e966011f598910bcf7777e55c7dcd2d
yichienlu <yichien.lu89@gmail.com>
2023-01-30 17:28:21 +0800

    git add

---
+++ src/views/AboutView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      data:{}
+    }
+  },
+  mounted(){
+    console.log(import.meta.env.VITE_TEXT)
+    // console.log(import.meta.env.VITE_PATH)
+    const url = import.meta.env.VITE_PATH
+    this.$http.get(url)
+    .then((res)=>{
+      // console.log(res.data)
+      this.data = res.data.results[0].name
+    })
+  }
+}
+
+</script>
+
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
